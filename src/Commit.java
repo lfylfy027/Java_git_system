6985158c38b328dacfeb439475588cf6226654eb
Lin Fangyu <441227036@qq.com>
2021-01-04 17:24:50 +0800

    update

--- src/Commit.java
+++ src/Commit.java
@@ -1,24 +1,51 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Commit extends KeyValueObject {
 	//parent等信息的写入之后完善
 	protected String parent;
 	protected String author;
-	protected String commiter;
+	protected String committer;
+	protected String tree;
 	
-	
-	
+	public void loadcommit(File commit) {
+		try {
+			Scanner in=new Scanner(commit);
+			
+			String[] line=in.nextLine().split(" ");
+			this.tree=line[1];
+			
+			line=in.nextLine().split(" ");
+			if(line.length==2) {this.parent=line[1];}
+			
+			line=in.nextLine().split(" ");
+			if(line.length==2) {this.author=line[1];}
+			
+			line=in.nextLine().split(" ");
+			if(line.length==2) {this.committer=line[1];}
+			
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	//根据仓库路径新生成一个commit
 	public Commit(File dir) throws Exception {
 		this.File=dir;
 		this.Type = "commit";
 		this.Value="";
-		Value+= "tree "+KeyValue_Storage.storage(dir)+"\n";
+		this.tree=KeyValue_Storage.storage(dir);
+		Value+= "tree "+tree+"\n";
 		Value+= "parent "+"\n";
 		Value+= "author "+"\n";
-		Value+= "commiter "+"\n";
+		Value+= "committer "+"\n";
 		GenerateKey(Value);
+	}
+	
+	public String gettree() {
+		return tree;
 	}
 	
 	@Override
---
+++ src/Reset.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Reset {
+	protected Repository rep;
+	protected Commit commit;
+	protected String gitDir=set_gitDir();
+	protected String dir=set_dir();
+	
+	private String set_gitDir() {
+		return rep.gitDir;
+	}
+	
+	private String set_dir() {
+		return rep.dir;
+	}
+	
+	//递归实现回滚文件的方法
+	private void reset(String dirpath,String treekey) {
+		File tree=KeyValue_Storage.getValue(gitDir, treekey);
+		try {
+			Scanner in=new Scanner(tree);
+			while(in.hasNext()) {
+				String[] line=in.nextLine().split(" ");
+				String type=line[1];
+				String key=line[2];
+				String filename=line[3];
+				if(type.equals("tree")) {
+					File dir = new File(dirpath+"/"+filename);
+		            if (!dir.exists()) {
+		            	// 判断目录是否存在     
+		                dir.mkdir();   
+		            }
+					reset(dirpath+"/"+filename, key);
+				}
+				if(type.equals("blob")) {
+					String newpath=dirpath+"/"+filename;
+						try {
+							copyfile(KeyValue_Storage.getValue(gitDir, key), newpath);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//回滚时从objects中复制文件到项目文件夹
+	private static void copyfile(File oldfile,String newpath) throws IOException  {
+		FileInputStream fileInputStream = new FileInputStream(oldfile);
+        FileOutputStream fileOutputStream = new FileOutputStream(newpath);
+        byte[] buffer = new byte[1024];
+        while (fileInputStream.read(buffer) != -1) {
+        		fileOutputStream.write(buffer);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+	}
+	
+	private void cleardir() {
+		File dirfile=new File(dir);
+		File[] fs = dirfile.listFiles();
+		ArrayList<File> files=new ArrayList<File>(Arrays.asList(fs));
+		for(int i = 0; i < files.size(); i++) {
+			if(files.get(i).getName()!=".git")files.get(i).delete();
+		}
+	}
+	
+	public void reset_hard() {
+		cleardir();
+		reset(dir,commit.gettree());
+	}
+}
