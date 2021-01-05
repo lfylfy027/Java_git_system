0aa34a304c6162659e80ed4baaede339b7f6422e
yyk776 <2000022776@stu.pku.edu.cn>
2021-01-05 19:53:42 +0800

    修复bug

--- src/operationobjects/Branch.java
+++ src/operationobjects/Branch.java
@@ -1,29 +1,37 @@
 package operationobjects;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import keyvalueobjects.Commit;
 
 public class Branch {
     String branchName = "master";
-    String Path;
-    String CommitID;
        
     //新建分支的构造方法
-    public Branch(String name1, String gitDir, String commitid) throws IOException {
+    public Branch(String name1, String gitDir, String commitKey) throws IOException {
     	this.branchName = name1;
-    	this.Path = gitDir;
-    	writeCommitID(commitid);    	
+    	writeCommitKey(commitKey,gitDir);    	
     }
     
+
     //切换分支时的构造方法
     public Branch(String name, String gitDir) throws IOException {
     	this.branchName = name;
-    	this.Path = gitDir;
-    	@SuppressWarnings("resource")
-		BufferedReader in = new BufferedReader(new FileReader(Path + "/refs/heads/" + name));
-    	writeCommitID(in.readLine());     	    	
+    	File in = new File(gitDir + "/refs/heads/master");
+		if(in.length() != 0) {
+			@SuppressWarnings("resource")
+			BufferedReader input = new BufferedReader(new FileReader(gitDir + "/refs/heads/" + name));   	
+	    	writeCommitKey(input.readLine(), gitDir);					
+		}
+    	     	    	
     }
         
     public Branch() {
@@ -34,44 +42,46 @@ public class Branch {
     	return branchName;
     }
     
-    public void writeCommitID(String commitid, String commitinfo) throws IOException {    	
+	//有commit的情况
+    public void writeCommitKey(String commitKey, String commitinfo, String gitDir) throws IOException {    	
     	//在分支文件中写入指向的commit
-    	CommitID = commitid;
-        File f = new File(Path + "/refs/heads/" + branchName);
+        File f = new File(gitDir + "/refs/heads/" + branchName);
         FileWriter p = new FileWriter(f);
-        p.write(CommitID);
+        p.write(commitKey);
         p.close();
         
         //在logs中追加commit记录
-        File logf = new File(Path + "/logs/refs/heads/" + branchName);
+        File logf = new File(gitDir + "/logs/refs/heads/" + branchName);
     	FileWriter logp = new FileWriter(logf , true);
     	logp.write(commitinfo+"\n");
     	logp.close();        
     }
     
-	private void writeCommitID(String commitID2) throws IOException {
+    //没有commit的情况
+	public void writeCommitKey(String commitKey, String gitDir) throws IOException {
     	//在分支文件中写入指向的commit
-    	CommitID = commitID2;
-        File f = new File(Path + "/refs/heads/" + branchName);
+        File f = new File(gitDir + "/refs/heads/" + branchName);
         FileWriter p = new FileWriter(f);
-        p.write(CommitID);
+        p.write(commitKey);
         p.close();
 	}
 
     //将分支重新指向一个commit
-    public void resetBranch(String CommitID1) throws IOException {
-    	CommitID = CommitID1;
-    	writeCommitID(CommitID);	
+    public void resetBranch(String commitKey, String gitDir) throws IOException {
+    	writeCommitKey(commitKey,gitDir);	
     }
     
-    //获取CommitID
-    public String getCommitID() {
-    	return CommitID;
+    //获取CommitKey
+    public String getCommitKey(String gitDir) throws IOException {
+    	BufferedReader in = new BufferedReader(new FileReader(gitDir + "/refs/heads/" + branchName));
+    	String CommitKey = in.readLine();
+    	in.close();
+    	return CommitKey;
     }
     
 	//删除某commit记录以后的所有记录
-	public void resetCommitHistory(String commitkey) throws IOException {
-		String pathdir=Path + "/logs/refs/heads/" + branchName;
+	public void resetCommitHistory(String commitkey, String gitDir) throws IOException {
+		String pathdir=gitDir + "/logs/refs/heads/" + branchName;
 		System.out.print(pathdir);
 		File logf = new File(pathdir);
 		BufferedReader br = new BufferedReader(new FileReader(logf));
@@ -89,12 +99,50 @@ public class Branch {
 		br.close();
 		logf.delete();
 		String[] newCommits = usefulCommits.split("\\|");				
-		File newlogf = new File(Path + "/logs/refs/heads/" +branchName);
+		File newlogf = new File(gitDir + "/logs/refs/heads/" +branchName);
 		FileWriter logp = new FileWriter(newlogf , true);
 		for(int i = 0; i<newCommits.length; i++) {
 			logp.write(newCommits[i]+"\n");
 			System.out.println(newCommits[i]);
 		}
     	logp.close(); 
+	}
+	
+	//切换分支
+	public void alterBranch(String name, String gitDir) throws IOException {
+		File f = new File(gitDir + "/refs/heads/" + name);	
+        if (f.exists()) {
+        	
+        	//重写HEAD
+        	File HEAD = new File(gitDir + "/HEAD");
+            FileWriter p = new FileWriter(HEAD);
+        	p.write("ref: refs/heads/" + name);
+        	p.close();
+        	
+        	//在logs/HEAD中追加切换分支记录
+        	File logHEAD = new File(gitDir + "/logs/HEAD");
+        	FileWriter p1 = new FileWriter(logHEAD , true);
+        	p1.write("ref: refs/heads/" + name + "\n");
+        	p1.close();  
+        	
+        	branchName = name;
+        	      	
+        }
+        else System.out.println("The branch doesn't exist");		
+	}
+	
+	//logs中的分支内容复制
+	public void copyLogs(String newBranch, String gitDir) throws IOException {
+		File prelog = new File(gitDir + "/logs/refs/heads/" + branchName);
+		InputStream input = new FileInputStream(prelog);
+		File newlog = new File(gitDir + "/logs/refs/heads/" + newBranch);
+		OutputStream output = new FileOutputStream(newlog);
+		byte[] buf = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = input.read(buf)) != -1) {
+			output.write(buf, 0 ,bytesRead);
+		}
+		input.close();
+		output.close();
 	}
 }
