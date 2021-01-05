140ac850abc14e3ab60bc771e56c458dec4a76b1
luohaoming98 <70061128+luohaoming98@users.noreply.github.com>
2021-01-05 15:50:24 +0800

    CommandLine V4
    @js00070

--- src/operationobjects/CommandLine.java
+++ src/operationobjects/CommandLine.java
@@ -5,60 +5,49 @@ import java.util.*;
 
 public class CommandLine {
 	public static void main(String[] args) throws Exception {
+		Operation go = new Operation();
 		while (true) {
 			Scanner input = new Scanner(System.in);
-	        String command = input.nextLine();
-	        
-	        String[] commandLine = command.split(" ");
-	        
-	    	if (commandLine[0].equals("init")){
-	    		new Operation().newRep(commandLine[1]);
-	    		System.out.println("新建仓库成功");
+			String command = input.nextLine();
+			String[] commandLine = command.split(" ");
 
-	    		
-	         }
-	    	
-	    	else if (commandLine[0].equals("commit")) {
-	    		new Operation().newCommit(commandLine[1]);
-	    		
-	            System.out.println("Commit成功");
-	        }
-	    	
-	        else if (commandLine[0].equals("log")){
-	        	new Operation().showBranches();
-	        	
-	        }
-	    	
-	        else if (commandLine[0].equals("reset")){
-	        	if (commandLine[1].equals("soft")){
-	            
-	        		
-	            }
-	        	else if (commandLine[1].equals("hard")){
-	            
-	        		
-	            }
-	        	else {
-	        		
-	        		
-	        	}
-	        }
-	    	
-	        else if (commandLine[0].equals("branch")){
-	        	new Operation().newBranch(commandLine[1]);
-	        	
-	        	System.out.println("新建分支成功");
-	        }
-	    	
-	        else if (commandLine[0].equals("checkout")){
-	        	new Operation().alterBranch(commandLine[1]);
-	        	
-	        	System.out.println("切换分支成功");
-	        }
-	    	
-	        else{
-	            System.out.println("输入指令错误");
-	        }
-	    }
+			if (commandLine[0].equals("gitto") && commandLine[1].equals("init")) {
+				go.newRep(commandLine[2]);
+				System.out.println("新建仓库成功");
+			}
+
+			else if (commandLine[0].equals("gitto") && commandLine[1].equals("commit")) {
+				go.newCommit(commandLine[2]);
+				System.out.println("Commit成功");
+			}
+
+			else if (commandLine[0].equals("gitto") && commandLine[1].equals("branch")) {
+				go.newBranch(commandLine[2]);
+				System.out.println("新建分支成功");
+			}
+
+			else if (commandLine[0].equals("gitto") && commandLine[1].equals("branch") && commandLine[2].equals("-a")) {
+				System.out.println("当前的分支有：");
+				go.showBranches();
+			}
+			
+			else if (commandLine[0].equals("gitto") && commandLine[1].equals("checkout")) {
+				go.alterBranch(commandLine[2]);
+				System.out.println("切换分支成功");
+			}
+
+			else if (commandLine[0].equals("gitto") && commandLine[1].equals("log")) {
+
+			}
+
+			else if (commandLine[0].equals("gitto") && commandLine[1].equals("reset")) {
+				go.reset_hard(commandLine[2]);
+				System.out.println("回滚成功");
+			}
+
+			else {
+				System.out.println("输入指令错误");
+			}
+		}
 	}
 }
