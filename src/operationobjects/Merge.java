80377a1876f1d38a3a2a624f406523da59c16a79
Lin Fangyu <441227036@qq.com>
2021-01-06 12:58:15 +0800

    完善了merge功能

--- src/operationobjects/Merge.java
+++ src/operationobjects/Merge.java
@@ -1,12 +1,17 @@
 package operationobjects;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 import keyvalueobjects.Commit;
+import keyvalueobjects.KeyValue_Storage;
 
 public class Merge extends GitOperation {
 	protected Branch newbranch;
@@ -15,6 +20,11 @@ public class Merge extends GitOperation {
 		super(rep, branch, commit);
 		this.newbranch=newbranch;
 	}
+	/*
+		判断需要merge的两分支的最近共同祖先，如果是共同祖先是base分支，则base分支直接添加new分支的
+	最新commit；如果共同祖先是new分支，则说明base分支更靠前，不需变动；如果共同祖先是第三个commit，则
+	进行三路合并，保留相对共同祖先的变动。如果同一文件发生了不同的变动，则返回错误信息。
+	*/
 	public String checkcommit() {
 		ArrayList<String> listbase=new ArrayList<String>();
 		ArrayList<String> listnew=new ArrayList<String>();
@@ -36,6 +46,80 @@ public class Merge extends GitOperation {
 			return "fast";
 		}
 		return commonCommit;
+	}
+	
+	//三路合并
+	public void three_merge(String commoncommit) {
+		Commit newcommit=new Commit();
+		Commit basecommit=new Commit();
+		Commit thirdcommit=new Commit();
+		try {
+			newcommit.loadcommit(gitDir, newbranch.getCommitKey(gitDir));
+			basecommit.loadcommit(gitDir, branch.getCommitKey(gitDir));
+			thirdcommit.loadcommit(gitDir, commoncommit);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		File basetree=KeyValue_Storage.getValue(gitDir, basecommit.gettree());
+		File newtree=KeyValue_Storage.getValue(gitDir, newcommit.gettree());
+		File thirdtree=KeyValue_Storage.getValue(gitDir, thirdcommit.gettree());
+		String [][] base=new String[10][];
+		String [][] newf=new String[10][];
+		String [][] third=new String[10][];
+		Scanner in0;
+		try {
+			in0 = new Scanner(basetree);
+		int i=0;
+		while(in0.hasNext()) {
+			base[i]=in0.nextLine().split(" ");
+			i++;
+		}
+		in0.close();
+		Scanner in1=new Scanner(newtree);
+		i=0;
+		while(in1.hasNext()) {
+			newf[i]=in1.nextLine().split(" ");
+			i++;
+		}
+		in1.close();
+		Scanner in2=new Scanner(thirdtree);
+		i=0;
+		while(in2.hasNext()) {
+			third[i]=in2.nextLine().split(" ");
+			i++;
+		}
+		in2.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//如果new分支的文件与base分支和共同祖先都不同，则保留。
+		for (String[] strings : newf) {
+			if(strings==null)break;
+			if(!findfile(base, strings[3], strings[2])&&!findfile(third, strings[3], strings[2])) {
+				try {
+					copyfile(KeyValue_Storage.getValue(gitDir, strings[2]), dir+"/"+strings[3]);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		//如果base分支中的文件与祖先相同，与new分支不同，则删除，保留new分支的相应文件
+		for (String[] strings : base) {
+			if(strings==null)break;
+			if(findfile(third, strings[3], strings[2])&&!findfile(newf, strings[3], strings[2])) {
+				File file=new File(dir+"/"+strings[3]);
+				file.delete();
+			}
+		}
+	}
+	private boolean findfile(String[][] lists,String target,String key) {
+		for (String[] list : lists) {
+			if(list==null)return false;
+			if(list[3].equals(target)&&list[2].equals(key))return true;
+		}
+		return false;
 	}
 	
 	private ArrayList<String> getCommitlist(Branch branch){
@@ -69,15 +153,30 @@ public class Merge extends GitOperation {
 			e.printStackTrace();
 		}
 	}
-	public String merge() {
+	
+	//如果merge的当前分支与
+	public int merge() {
 		String tmp=checkcommit();
-		if(tmp.equals("uptodate"))return "当前分支领先于目标分支";
+		if(tmp.equals("uptodate"))return 1;
 		if(tmp.equals("fast")) {
 			fast_merge();
-			return "分支合并成功";
+			return 0;
 		}
-		return "";
+		three_merge(tmp);
+		return 2;
 	}
+	
+	//从objects中复制文件到项目文件夹
+		private static void copyfile(File oldfile,String newpath) throws IOException  {
+			FileInputStream fileInputStream = new FileInputStream(oldfile);
+	        FileOutputStream fileOutputStream = new FileOutputStream(newpath);
+	        byte[] buffer = new byte[1024];
+	        while (fileInputStream.read(buffer) != -1) {
+	        		fileOutputStream.write(buffer);
+	        }
+	        fileInputStream.close();
+	        fileOutputStream.close();
+		}
 	public static void main(String[] args) {
 	}
 }
